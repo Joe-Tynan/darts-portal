f8f0e67bc623fedaaf502668c9e9fc6db6c72fca
Joe Tynan <joe.tynan@pixelkicks.co.uk>
2024-01-11 12:04:35 +0000

    Filtering

--- darts/views.py
+++ darts/views.py
@@ -14,20 +14,20 @@ class LeagueTableView(ListView):
     context_object_name = 'players'
     
     def get_queryset(self):
-        return CustomUser.objects.annotate(num_wins=Count('wins'), last_win=Max('wins__date')).order_by('-num_wins', '-last_win').distinct()
+        return CustomUser.objects.annotate(num_wins=Count('wins'), last_win=Max('wins__date')).order_by('-num_wins', 'last_win').distinct()
 
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
         
         # Add in custom QuerySets
-        context["champions_league_players"] = CustomUser.objects.annotate(num_wins=Count('wins', distinct=True), last_win=Max('wins__date')).order_by('-num_wins', '-last_win').distinct()[:4]
-        context["premier_league_players"] = CustomUser.objects.annotate(num_wins=Count('wins', distinct=True), last_win=Max('wins__date')).order_by('-num_wins', '-last_win').distinct()[4:8]
-        context["championship_players"] = CustomUser.objects.annotate(num_wins=Count('wins', distinct=True), last_win=Max('wins__date')).order_by('-num_wins', '-last_win').distinct()[8:12]
-        context["vanarama_players"] = CustomUser.objects.annotate(num_wins=Count('wins', distinct=True), last_win=Max('wins__date')).order_by('-num_wins', '-last_win').distinct()[12:16]
+        context["champions_league_players"] = CustomUser.objects.annotate(num_wins=Count('wins', distinct=True), last_win=Max('wins__date')).order_by('-num_wins', 'last_win').distinct()[:4]
+        context["premier_league_players"] = CustomUser.objects.annotate(num_wins=Count('wins', distinct=True), last_win=Max('wins__date')).order_by('-num_wins', 'last_win').distinct()[4:8]
+        context["championship_players"] = CustomUser.objects.annotate(num_wins=Count('wins', distinct=True), last_win=Max('wins__date')).order_by('-num_wins', 'last_win').distinct()[8:12]
+        context["vanarama_players"] = CustomUser.objects.annotate(num_wins=Count('wins', distinct=True), last_win=Max('wins__date')).order_by('-num_wins', 'last_win').distinct()[12:16]
 
-        context["monthly_leaders"] = CustomUser.objects.annotate(num_wins=Count('wins'), last_win=Max('wins__date')).filter(wins__date__contains=datetime.date.today().month).order_by('-num_wins', '-last_win').distinct()
-        context["weekly_leaders"] = CustomUser.objects.annotate(num_wins=Count('wins'), last_win=Max('wins__date')).filter(wins__date__week=datetime.date.today().isocalendar()[1]).order_by('-num_wins', '-last_win').distinct()
+        context["monthly_leaders"] = CustomUser.objects.annotate(num_wins=Count('wins'), last_win=Max('wins__date')).filter(wins__date__contains=datetime.date.today().month).order_by('-num_wins', 'last_win').distinct()
+        context["weekly_leaders"] = CustomUser.objects.annotate(num_wins=Count('wins'), last_win=Max('wins__date')).filter(wins__date__week=datetime.date.today().isocalendar()[1]).order_by('-num_wins', 'last_win').distinct()
 
         context["this_years_game_count"] = Win.objects.filter(date__contains=datetime.date.today().year).count()
 
