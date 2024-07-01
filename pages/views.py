cd2dc772274034261e722bfb8eeb09a4a18f89c3
Joe Tynan <joe.tynan@pixelkicks.co.uk>
2024-07-01 20:32:40 +0100

    Testing Vue & Webpack

--- pages/views.py
+++ pages/views.py
@@ -2,3 +2,6 @@ from django.views.generic import TemplateView
 
 #class RoadmapPageView(TemplateView):
     #template_name = 'roadmap.html'
+
+class WebpackPageView(TemplateView):
+    template_name = 'pages/webpack.html'
