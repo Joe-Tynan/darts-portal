8a880711c3f6a614cae97f3c708d916263f06107
Joe Tynan <joe.tynan@pixelkicks.co.uk>
2025-01-30 11:22:46 +0000

    Submit Form Validation Improvements

--- darts/forms.py
+++ darts/forms.py
@@ -1,15 +1,57 @@
 from django import forms
+from django.core.exceptions import ValidationError
+from django.utils.timezone import now
 
 from users.models import CustomUser
 from .models import Win
 
 class WinCreateForm(forms.ModelForm):
+    winner = forms.ModelChoiceField(
+        queryset=CustomUser.objects.exclude(plays_darts=False),
+        required=True
+    )
+    runner_up = forms.ModelChoiceField(
+        queryset=CustomUser.objects.exclude(plays_darts=False),
+        required=True
+    )
     participants = forms.ModelMultipleChoiceField(
         queryset=CustomUser.objects.exclude(plays_darts=False),
         widget=forms.CheckboxSelectMultiple(),
         label="All Participants",
+        required=True
     )
 
     class Meta:
         model = Win
         fields = ['winner', 'runner_up', 'participants']
+
+    def clean(self):
+        # Overall Form Validation
+        cleaned_data = super().clean()
+        winner = cleaned_data.get('winner')
+        runner_up = cleaned_data.get('runner_up')
+        participants = cleaned_data.get('participants')
+
+        if not participants:
+            raise ValidationError('You obviously need to add participants you weapon!')
+
+        # 1. The winner and runner up can't be the same
+        if winner and runner_up and winner == runner_up:
+            raise ValidationError('The winner and runner up have to be different people you donut!')
+
+        # 2. Winner and runner up must be selected as participants
+        if winner and winner not in participants:
+            raise ValidationError('What are you doing? Include the winner in the participants!')
+
+        if runner_up and runner_up not in participants:
+            raise ValidationError('What are you doing? Include the runner up in the participants!')
+
+        # 3. At least 3 participants must be selected
+        if participants and len(participants) < 3:
+            raise ValidationError('Stop trying to get an easy point Bray. There has to be at least 3 people in a game!')
+
+        #4 Only one submission can be done per day
+        if Win.objects.filter(date=now().date()).exists():
+            raise ValidationError('A game has already been submitted today. Pay attention you wet wipe!')
+
+        return cleaned_data
--- static/js/script.js
+++ static/js/script.js
@@ -2,52 +2,96 @@ let sidebars = document.querySelectorAll('.sidebar');
 let cards = document.querySelectorAll('.player-card');
 let closeButtons = document.querySelectorAll('.button-close');
 
-for(let i = 0; i < cards.length; i++) {
-    cards[i].addEventListener('click', function() {
-        for(let j = 0; j < sidebars.length; j++ ) {
-            sidebars[j].classList.remove('active');
-        }
-        sidebars[i].classList.add('active');
-    });
+if( cards ) {
+    for(let i = 0; i < cards.length; i++) {
+        cards[i].addEventListener('click', function() {
+            for(let j = 0; j < sidebars.length; j++ ) {
+                sidebars[j].classList.remove('active');
+            }
+            sidebars[i].classList.add('active');
+        });
+    }
 }
 
-for(let i = 0; i < closeButtons.length; i++) {
-    closeButtons[i].addEventListener('click', function() {
-        for(let j = 0; j < sidebars.length; j++ ) {
-            sidebars[j].classList.remove('active');
-        }
-    });
+if( closeButtons ) {
+    for(let i = 0; i < closeButtons.length; i++) {
+        closeButtons[i].addEventListener('click', function() {
+            for(let j = 0; j < sidebars.length; j++ ) {
+                sidebars[j].classList.remove('active');
+            }
+        });
+    }
 }
 
 let allGamesTable = document.querySelectorAll('table tr');
 
-for(let i = 0; i < allGamesTable.length; i++) {
-    let lastLoser = allGamesTable[i].querySelector('td:last-of-type span:last-of-type');
+if( allGamesTable ) {
+    for(let i = 0; i < allGamesTable.length; i++) {
+        let lastLoser = allGamesTable[i].querySelector('td:last-of-type span:last-of-type');
 
-    if( lastLoser ) {
-        html = lastLoser.innerHTML;
-        html = html.replace(", ", "");
+        if( lastLoser ) {
+            html = lastLoser.innerHTML;
+            html = html.replace(", ", "");
 
-        lastLoser.innerHTML = html;
+            lastLoser.innerHTML = html;
+        }
     }
 }
 
 let modalToggle = document.querySelector('#toggle-modal');
 
-modalToggle.addEventListener('click', function() {
-    let body = document.querySelector('body');
-    let modal = document.querySelector('.modal-section');
+if( modalToggle ) {
+    modalToggle.addEventListener('click', function() {
+        let body = document.querySelector('body');
+        let modal = document.querySelector('.modal-section');
 
-    modal.classList.add('active');
-    body.classList.add('modal-open');
-});
+        modal.classList.add('active');
+        body.classList.add('modal-open');
+    });
+}
 
 let closeModalButton = document.querySelector('#close-modal');
 
-closeModalButton.addEventListener('click', function() {
-    let body = document.querySelector('body');
-    let modal = document.querySelector('.modal-section');
+if( closeModalButton ) {
+    closeModalButton.addEventListener('click', function() {
+        let body = document.querySelector('body');
+        let modal = document.querySelector('.modal-section');
 
-    modal.classList.remove('active');
-    body.classList.remove('modal-open');
-});
+        modal.classList.remove('active');
+        body.classList.remove('modal-open');
+    });
+}
+
+// Auto Selecting Winner and Runner Up in Participants Field
+let winnerField = document.querySelector('#id_winner');
+let runnerUpField = document.querySelector('#id_runner_up');
+let participantsCheckboxes = document.querySelectorAll('.checkbox-wrapper .checkbox');
+
+if( winnerField ) {
+    winnerField.addEventListener('change', function() {
+        //console.log(`Winner Changed: ${winnerField.value}`);
+        let winner_id = winnerField.value;
+
+        for(let i=0; i < participantsCheckboxes.length; i++ ) {
+            //console.log(participantsCheckboxes[i].children.participants.value);
+            if( participantsCheckboxes[i].children.participants.value === winner_id ) {
+                //console.log(`${participantsCheckboxes[i].children.participants.value} matches!`);
+                participantsCheckboxes[i].children.participants.checked = true;
+            }
+        }
+    });
+}
+
+if( runnerUpField ) {
+    runnerUpField.addEventListener('change', function() {
+        let runner_up_id = runnerUpField.value;
+
+        for(let i=0; i < participantsCheckboxes.length; i++ ) {
+            //console.log(participantsCheckboxes[i].children.participants.value);
+            if( participantsCheckboxes[i].children.participants.value === runner_up_id ) {
+                //console.log(`${participantsCheckboxes[i].children.participants.value} matches!`);
+                participantsCheckboxes[i].children.participants.checked = true;
+            }
+        }
+    });
+}
